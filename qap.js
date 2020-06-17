c10faef6b062252e3621a253bdb3d7ba94226a01
Rafie Walker <slickytail.mc@gmail.com>
2020-06-17 06:45:20 -0700

    Working version

---
+++ qap.js
@@ -0,0 +1,60 @@
+class Qap {
+	constructor() {
+		this.points = new Set();
+		this.exclude = {}
+	}
+
+	add(point) {
+		if (point >= Math.pow(2, this.dim))
+			throw new RangeError(`Point too large for dimension ${this.dim}`);
+		if (this.exclude[point])
+			throw new RangeError("Point excluded");
+		// If we already have the point, just silently return
+		// If we try to create excludes with a point we already have, we'll mess up our cap
+		if (this.points.has(point))
+			return;
+		// Update the exclude
+		for (let p1 of this.points) {
+			for (let p2 of this.points) {
+				if (p1 < p2) {
+					let exc = point^p1^p2;
+					if (!this.exclude[exc])
+						this.exclude[exc] = 0;
+					this.exclude[exc]++;
+				}
+			}
+		}
+		// Add the point
+		this.points.add(point)
+	}
+	remove(point) {
+		if (!this.points.has(point))
+			return;
+		this.points.delete(point);
+		// Trim the excludes.
+		for (let p1 of this.points) {
+			for (let p2 of this.points) {
+				if (p1 < p2)
+					this.exclude[point^p1^p2]--;
+			}
+		}
+	}
+	contains(point) {
+		return this.points.has(point);
+	}
+	excludes(point) {
+		if (!this.exclude[point])
+			this.exclude[point] = 0;
+		return this.exclude[point];
+	}
+	clear() {
+		this.points = new Set();
+		this.exclude = {};
+	}
+	changeDim(newDim) {
+		for (let p of this.points) {
+			if (p > Math.pow(2**newDim))
+				this.remove(p);
+		}
+	}
+}
--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-var dim = 3;
+// Looking at caps in Z_2^dim
+var dim = 4;
+// Should the grid be drawn with four components? (vs two)
 var quadStyle = true;
-var qap = {};
+// The points currently in the qap
+var qap = new Qap();
 function createListeners() {
 	// Dimension chooser
 	let dimDown = document.getElementById("dim-down");
 	let dimUp = document.getElementById("dim-up");
 	let dimText = document.getElementById("dim");
 
-	const minDim = 1;
+	const minDim = 2;
 	const maxDim = 10;
 	function updateDimText() {
 		if (dim <= minDim) {
@@ -24,10 +27,11 @@ function createListeners() {
 			dimUp.classList.remove("disabled");
 		}
 		dimText.innerText = dim;
-		setTimeout(drawQap);
+		qap.changeDim(dim);
+		setTimeout(drawAffSpace);
 	}
-	dimDown.onclick = () => {dim--; updateDimText();}
-	dimUp.onclick = () => {dim++; updateDimText();}
+	dimDown.onclick = () => {dim--; updateDimText()};
+	dimUp.onclick = () => {dim++; updateDimText()};
 
 	// Dimension display
 	let z2 = document.getElementById("z2");
@@ -42,7 +46,7 @@ function createListeners() {
 			z2.classList.add("selected");
 			z4.classList.remove("selected");
 		}
-		setTimeout(drawQap);
+		setTimeout(drawAffSpace);
 	}
 	z2.onclick = () => setQuadStyle(false);
 	z4.onclick = () => setQuadStyle(true);
@@ -51,16 +55,149 @@ function createListeners() {
 	// Qap clear
 	let clear = document.getElementById("clear");
 	clear.onclick = () => {
-		qap = {};
-		setTimeout(drawQap);
+		qap.clear();
+		setTimeout(drawAffSpace);
 	}
 
-	setTimeout(drawQap);
+	setTimeout(drawAffSpace);
 }
+function drawAffSpace() {
+	let qapSvg = document.getElementById("qap");
+	// Draw the grid
+	// First, we have to compute the size of the grid.
 
-function drawQap() {
-	let qap = document.getElementById("qap");
-	// Draw a grid
+	// The viewbox is set to 100 x 100. (maybe plus padding)
+	// In the future, we should actually change the viewBox here, depending on the quadStyle and the parity of dim
+	const w = 100;
+	const h = 100;
+
+	// We'll put the odd dimension on the X-axis. (this should probably depend on viewport orientation.)
+	// First, we'll figure out the Z2-style layout (ie, quadStyle == false)
+	let xDim = Math.ceil(dim/2);
+	let yDim = Math.floor(dim/2);
+	let xL = Math.pow(2, xDim);
+	let yL = Math.pow(2, yDim);
+	// We want our cells to actually be square. 
+	let cellSize = Math.min(w / xL, h / yDim);
+	// We're going to vertically center the cells.
+	let gridHeight = cellSize * yL;
+	let gridYMin = (h - gridHeight)/2;
+	let gridYMax = (h + gridHeight)/2;
+
+	// Draw lines
+	// Generate priority (ruler tickmarks) for lines
+	let ruler = new Array(xL+1).fill(0);
+	for (let w = 0; w < xDim; w++) {
+		let wavelength = Math.pow(2, w);
+		for (let i = 0; i <= xL; i += wavelength) {
+			ruler[i]++;
+		}
+	}
+	const rulerColors = ["#333", "#444", "#666", "#567", "#46a", "#38d"];
+	const colorScale = d3.interpolateRgbBasis(rulerColors);
+
+	let lines = new Array();
+	for (let xi = 0; xi <= xL; xi++) {
+		lines.push({
+			vert: true,
+			x: xi,
+			priority: (xi != 0 && xi != xL) ? ruler[xi] : 0.5
+		});
+	}
+	for (let yi = 0; yi <= yL; yi++) {
+		lines.push({
+			vert: false,
+			y: yi,
+			priority: (yi != 0 && yi != yL) ? ruler[yi] : 0.5
+		});
+	}
+
+	lines.sort((a, b) => a.priority - b.priority);
+
+	let grid = d3.select(qapSvg)
+		.select("#grid")
+	    .selectAll(".line")
+	    .data(lines);
+
+	let enter = grid.enter().append("line")
+	    .classed("line", true);
+	grid.exit().remove();
+
+	grid = grid.merge(enter)
+	    .attr("x1", d =>  d.vert ? d.x * cellSize : 0)
+	    .attr("x2", d =>  d.vert ? d.x * cellSize : 100)
+	    .attr("y1", d => !d.vert ? gridYMin + d.y * cellSize : gridYMin)
+	    .attr("y2", d => !d.vert ? gridYMin + d.y * cellSize : gridYMax)
+	    .attr("stroke-width", d => (d.priority+1)*Math.sqrt(cellSize)/15)
+	    .attr("stroke", d => colorScale(d.priority / xDim));
+
+	// Draw squares for getting clicks
+	let squares = new Array();
+	for (let xi = 0; xi < xL; xi++) {
+		for (let yi = 0; yi < yL; yi++) {
+			// Compute which SET card this actually is, in Z2 notation
+			// The strategy here is to interleave the bits of xi and yi.
+			let cardNum = 0;
+			for (let i = 0; i < xDim; i++) {
+				// The ith-leftmost bit of xi is 
+				let bx = (xi & (1 << i)) >>> i;
+				let by = (yi & (1 << i)) >>> i;
+				// Now we need to set the 2ith bit of cardnum to bx and the (2i+1)th to by
+				cardNum = cardNum | (bx << (2*i)) | (by << (2*i + 1));
+			}
+			squares.push({
+				xi: xi,
+				yi: yi,
+				card: cardNum
+			})
+		}
+	}
+
+	let cards = d3.select(qapSvg)
+		.select("#points")
+	    .selectAll(".card")
+	    .data(squares, d => d.card);
+
+	let dCards = cards.enter().append("g")
+	    .classed("card", true)
+	    .on('click', function(d) {
+	    	if (qap.excludes(d.card))
+	    		return;
+	    	if (qap.contains(d.card))
+	    		qap.remove(d.card);
+	    	else
+	    		qap.add(d.card);
+	    	drawQap();
+	    });
+	dCards.append("rect");
+	dCards.append("text");
+	cards.exit().remove();
+
+	cards = cards.merge(dCards)
+		.attr("transform", d => `translate(${d.xi * cellSize}, ${gridYMin + d.yi * cellSize})`);
+	cards.select("rect")
+	    .attr("width", cellSize)
+	    .attr("height", cellSize);
+	cards.select("text")
+		.attr("font-size", cellSize*0.9)
+		.attr("x", cellSize/2)
+		.attr("y", cellSize/2);
+
+	function drawQap() {
+		cards
+		    .classed("in-qap", d => qap.contains(d.card))
+		    .classed("excluded", d => qap.excludes(d.card));
+		cards.select("text")
+			.text(function(d) {
+				if (qap.excludes(d.card))
+					return qap.excludes(d.card);
+				if (qap.contains(d.card))
+					return "⯁";
+				return;
+		})
+	}
+	drawQap();
+
 }
 
 if (document.readyState === "complete" ||
