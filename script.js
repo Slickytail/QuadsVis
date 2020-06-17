cebce60dce2e3a2e75860d4906fbf40b122a5a9e
Rafie Walker <slickytail.mc@gmail.com>
2020-06-17 00:51:19 -0700

    Add structure

---
+++ script.js
@@ -0,0 +1,10 @@
+function createListeners() {
+	
+}
+
+if (document.readyState === "complete" ||
+   (document.readyState !== "loading" && !document.documentElement.doScroll) ) {
+	createListeners();
+} else {
+	document.addEventListener("DOMContentLoaded", createListeners);
+}
